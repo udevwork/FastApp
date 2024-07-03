ef563c9fd55ff6f2518cf7ebceba054a85233304
Denis <udevwork@gmail.com>
2024-07-03 17:55:04 +0200

    init commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "FastAppLibrary",
+    platforms: [
+          .macOS(.v10_15),
+          .iOS(.v17)
+      ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- Sources/FastAppLibrary/FastAppLibrary.swift
+++ Sources/FastAppLibrary/FastAppLibrary.swift
@@ -1,2 +1,12 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+struct OnboardingUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    OnboardingUIView()
+}
+
